1ad89008e47fe8ca2fe8e8ad6da72051df55667b
Nour Mahmoud <nour.m.mahmod@gmail.com>
2025-06-14 21:28:22 +0300

    Features section

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,13 +1,14 @@
 import FlashDeals from "@/Components/flashDeals";
 import LandingPage from "@/Components/landing";
 import TopCategories from "@/Components/topCategoriesSection";
-
+import FeaturesSection from "@/Components/featuresSection";
 const Home = () => {
   return (
     <main className="bg-backGround max-lg:relative">
       <LandingPage />
       <FlashDeals />
       <TopCategories />
+      <FeaturesSection />
     </main>
   );
 };
