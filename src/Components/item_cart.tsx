b00dd797e04599647b7141146ad84fa8ab5cfe4b
Nour Mahmoud <nour.m.mahmod@gmail.com>
2025-06-22 17:13:31 +0300

    update

--- src/Components/item_cart.tsx
+++ src/Components/item_cart.tsx
@@ -30,12 +30,8 @@ export default function Item_cart({
     setCartItems((prev) => prev.filter((x) => x.id !== order.id));
   };
   return (
-    <div className="bg-white rounded-[10px] shadow flex gap-2 h-[150px] max-[424px]:h-auto  max-[424px]:flex-col max-[424px]:items-center max-[424px]:relative">
-      <img
-        src={order.image}
-        className="w-1/3 max-[424px]:w-1/2 lg:w-1/6 p-4"
-        alt=""
-      />
+    <div className="bg-white rounded-[10px] shadow flex gap-2 h-auto max-[424px]:h-auto  max-[424px]:flex-col max-[424px]:items-center max-[424px]:relative">
+      <img src={order.image} className="w-1/6 max-[424px]:w-1/2 p-4" alt="" />
       <div className="p-4 flex w-full items-start justify-between gap-1">
         <div className="flex flex-col gap-2 h-full justify-between">
           <p className="text-lg">{order.title}</p>
